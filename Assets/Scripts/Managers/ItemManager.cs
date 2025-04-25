38e6cabd7f45c21c943a6c3ca659d17fdf283a1f
Josh Villgren <joshuavillgren@gmail.com>
2025-04-26 00:14:35 +1200

    added new templates and some extra params

--- Assets/Scripts/Managers/ItemManager.cs
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -13,14 +13,19 @@ namespace Managers
         public List<Item> GenerateItemsForCharacter(CharacterData character)
         {
             List<Item> joySparksItems = itemPool.Where(i  => character.likedItems.Contains(i)).ToList();
-            List<Item> joyNoSparksItems = itemPool.Where(i => i.tags.Any(t => character.dislikedTags.Contains(t))).ToList();
+            List<Item> joyNoSparksItems = itemPool.Where(i  => character.hatedItems.Contains(i)).ToList();
             List<Item> joyNoSparksTags = itemPool.Where(i => i.tags.Any(t => character.dislikedTags.Contains(t))).ToList();
 
             var mixed = new List<Item>();
-            mixed.AddRange(joySparksItems.OrderBy(x => Random.value).Take(3));
-            mixed.AddRange(joyNoSparksItems.OrderBy(x => Random.value).Take(2));
-            mixed.AddRange(joyNoSparksTags.OrderBy(x => Random.value).Take(2));
-
+            
+            Debug.Log("Trying to setup items for level"); // High chance of this crashing out atm
+            mixed.AddRange(joySparksItems.OrderBy(x => Random.value).Take(
+                Random.Range(character.minimumGoodItems, character.maximumGoodItems + 1)));
+            mixed.AddRange(joyNoSparksItems.OrderBy(x => Random.value).Take(
+                Random.Range(character.minimumBadItems, character.maximumBadItems + 1)));
+            mixed.AddRange(joyNoSparksTags.OrderBy(x => Random.value).Take(
+                Random.Range(0, character.maxDislikedTaggedItems + 1)));
+            Debug.Log("Successfully setup items for level");
             return mixed.OrderBy(x => Random.value).ToList(); // Shuffle
         }
     }
