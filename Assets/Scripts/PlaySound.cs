3f6153547662bfcc130c62144915c549a7d9a063
Josh Villgren <joshuavillgren@gmail.com>
2025-04-27 16:29:21 +1200

    add sfx on buttons

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,10 @@
+﻿using Audio;
+using Utils;
+
+public class PlaySound : Singleton<PlaySound>
+{
+        public void PlayASoundOnce(string soundName)
+        {
+                AudioWrapper.Instance.PlaySoundVoid(soundName);
+        }
+}
